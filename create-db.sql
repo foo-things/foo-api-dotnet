0cf2d0bcfd9b68d0fa0f281d8319f8068a72d082
Joe Garber <joegarb@users.noreply.github.com>
2019-06-16 20:50:46 +0100

    Run sql script on startup

--- create-db.sql
+++ create-db.sql
@@ -1,13 +1,20 @@
-CREATE DATABASE Foo
+IF DB_ID('Foo') IS NULL
+BEGIN
+   CREATE DATABASE Foo
+END
+
 GO
 USE Foo
 
-CREATE TABLE Thing (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    [Name] NVARCHAR(100) NOT NULL
-)
-INSERT INTO Thing ([Name]) 
-VALUES
-    ('asdf'),
-    ('fdsa'),
-    ('blah')
+IF (NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Thing'))
+BEGIN
+    CREATE TABLE Thing (
+        ID INT IDENTITY(1,1) PRIMARY KEY,
+        [Name] NVARCHAR(100) NOT NULL
+    )
+    INSERT INTO Thing ([Name]) 
+    VALUES
+        ('asdf'),
+        ('fdsa'),
+        ('blah')
+END
